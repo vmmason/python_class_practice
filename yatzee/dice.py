9d2f2d4bb2a76d228e6adb218587c8ffe8bbe07e
veronmmason@gmail.com <veronmmason@gmail.com>
2021-06-27 22:41:32 -0400

    Modified the dice program to include magic methods that evaluate values of die and can add die values together. It expands the functionality of our die rolling program.

--- yatzee/dice.py
+++ yatzee/dice.py
@@ -6,9 +6,35 @@ class Dice:
             raise ValueError("Must have at least two sides!")
         if not isinstance(sides, int):
             raise ValueError("Sides must be a whole number!")
-        
         self.value = value or random.randint(1, sides)
         
+    def __int__(self):
+        return self.value
+        
+    def __eq__(self, other):
+        return int(self) == other
+    
+    def __ne__(self, other):
+        return not int(self) == other
+    
+    def __gt__(self, other):
+        return int(self) > other
+    
+    def __lt__(self, other):
+        return int(self) < other
+    
+    def __ge__(self, other):
+        return int(self) > other or int(self) == other
+    
+    def __le__(self, other):
+        return int(self) < other or int(self) == other
+    
+    def __add__(self, other):
+        return int(self) + other
+    
+    def __radd__(self, other):
+        return int(self) + other
+    
         
 class D6(Dice):
     def __init__(self, value=0):

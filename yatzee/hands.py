93a9b43b534bb7aab6de310e42f2c6e264efafd2
veronmmason@gmail.com <veronmmason@gmail.com>
2021-06-28 22:07:49 -0400

    Completed the Treehouse videos on OOP, I will be circling back to Corey Schafer however. There were gaps in explanation and a lot of the coding didn't make sense for me.

--- yatzee/hands.py
+++ yatzee/hands.py
@@ -10,7 +10,64 @@ class Hands(list):
             self.append(die_class())
         self.sort()
         
+    def _by_value(self, value):
+        dice = []
+        for die in self:
+            if die == value:
+                dice.append(die)
+        return dice
+        
         
 class YatzeeHand(Hands):
     def __init__(self, *args, **kwargs):
         super().__init__(size=5, die_class=D6, *args, **kwargs)
+        
+    @property
+    def ones(self):
+        return self._by_value(1)
+    
+    @property
+    def twos(self):
+        return self._by_value(2)
+    
+    @property
+    def threes(self):
+        return self._by_value(3)
+    
+    @property
+    def fours(self):
+        return self._by_value(4)
+    
+    @property
+    def foves(self):
+        return self._by_value(5)
+    
+    @property
+    def sixes(self):
+        return self._by_value(6)
+    
+    @property
+    def _sets(self):
+        return {
+            1: len(self.ones),
+            2: len(self.twos),
+            3: len(self.threes),
+            4: len(self.fours),
+            5: len(self.fives),
+            6: len(self.sixes)
+            }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
